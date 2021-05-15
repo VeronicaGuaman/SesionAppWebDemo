78b17181033e0122af0d378f54a8ab26ef301c08
VeronicaGuaman <veronica_guaman1997@outlook.es>
2021-05-15 16:45:55 -0500

    Add controllers and views

---
+++ SesionAppWebDemo/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SesionAppWebDemo.Models;
+using SesionAppWebDemo.Models.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SesionAppWebDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ProductController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var product = _context.Products.ToList();
+            return View(product);
+        }
+
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SesionAppWebDemo/Migrations/20210515212629_InitialMigrations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SesionAppWebDemo.Migrations
+{
+    public partial class InitialMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    DateRegister = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
