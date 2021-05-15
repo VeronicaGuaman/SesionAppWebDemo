3b4cdb63ac16c2af93980a57a894dfbdc0dbe486
VeronicaGuaman <veronica_guaman1997@outlook.es>
2021-05-15 16:19:33 -0500

    Agregar archivos de proyecto.

---
+++ SesionAppWebDemo/Models/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SesionAppWebDemo.Models.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ SesionAppWebDemo/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SesionAppWebDemo.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public int Stock { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime DateRegister { get; set; }
+    }
+}
